d307f800c5eee76e96e212e8643eab1c6a168940
David Konsumer <konsumer@jetboystudio.com>
2015-11-07 22:59:50 -0800

    standard - singles are better than double-quotes

--- src/index.js
+++ src/index.js
@@ -5,8 +5,8 @@ import Deck from './Deck'
 import './index.css'
 
 ReactDOM.render((<div>
-  <h1><a href="mailto:konsumer@jetboystudio.com">David Konsumer</a>'s Shiftwise Exercise</h1>
-  <p>You can a zip of it <a href="https://github.com/konsumer/shiftwise-cards-react/archive/master.zip">here</a>, and browse the code on <a href="https://github.com/konsumer/shiftwise-cards-react">github</a>.</p>
+  <h1><a href='mailto:konsumer@jetboystudio.com'>David Konsumer</a>'s Shiftwise Exercise</h1>
+  <p>You can a zip of it <a href='https://github.com/konsumer/shiftwise-cards-react/archive/master.zip'>here</a>, and browse the code on <a href='https://github.com/konsumer/shiftwise-cards-react'>github</a>.</p>
   <Deck />
 </div>), document.getElementById('app'))
 
